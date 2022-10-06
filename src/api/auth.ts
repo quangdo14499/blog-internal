93a4ebea716bea31b82af0cc2a760f482fe92d71
Đo Minh Quang <quang.do@savvycomsoftware.com>
2022-10-06 13:43:22 +0700

    feature: Register + Setup Base

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,20 +1,20 @@
-import axiosClient from "./axiosClient";
-import { LoginInput, User } from "../interfaces/user";
-import urls from "../contants/urls";
+import axiosClient from './axiosClient'
+import { LoginInput, User } from '../interfaces/user'
+import urls from '../contants/urls'
 
 class Auth {
-  register = (body: User) =>{
+  register = (body: User) => {
     return axiosClient.post(`${urls.API_URL}auth/local/register`, {
       username: body.username,
       email: body.email,
-      password: body.password
-    });
-  };
-  
+      password: body.password,
+    })
+  }
+
   login = (body: LoginInput) => {
-    return axiosClient.post(`${urls.API_URL}auth/local`, body);
+    return axiosClient.post(`${urls.API_URL}auth/local`, body)
   }
 }
 
-const auth = new Auth();
-export default auth;
+const auth = new Auth()
+export default auth
